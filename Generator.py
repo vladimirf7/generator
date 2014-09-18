7d08cdd2ee893a09514ac78028e62aa9b055033e
Volodymyr Fesenko <volodymyr.fesenko@barclayscapital.com>
2014-09-18 11:01:59 +0300

    6

--- Generator.py
+++ Generator.py
@@ -1,76 +1,69 @@
-import os
 import sys
+import os
 import yaml
-
-class Table:
-    def __init__(self, name):
-        self._name = name
-        self._fields = {}
-        self._relations = []
+from table import Table
 
 CREATE_TABLE = """\
-CREATE TABLE \"{0}\" (
-    \"{0}_id\" SERIAL PRIMARY KEY,
+CREATE TABLE "{0}" (
+\t"{0}_id" SERIAL PRIMARY KEY,
 {1}
-    \"{0}_created\" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())\
-        AS INTEGER),
-    \"{0}_updated\" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())\
-        AS INTEGER)
+\t"{0}_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) \
+AS INTEGER),
+\t"{0}_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) \
+AS INTEGER)
 );\n\n"""
+
 CREATE_TRIGGER = """\
 CREATE OR REPLACE FUNCTION update_{0}_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-   NEW.{0}_updated = cast(extract(epoch from now()) as integer);
-   RETURN NEW;
+\tNEW.{0}_updated = cast(extract(epoch from now()) as integer);
+\tRETURN NEW;
 END;
-$$ language \'plpgsql\';
-CREATE TRIGGER "tr_{0}_updated" BEFORE UPDATE ON \"{0}\" FOR EACH ROW EXECUTE\
-    PROCEDURE update_{0}_timestamp();\n\n"""
+$$ language 'plpgsql';
+CREATE TRIGGER "tr_{0}_updated" BEFORE UPDATE ON "{0}" FOR EACH ROW \
+EXECUTE PROCEDURE update_{0}_timestamp();\n\n"""
 
-if len(sys.argv) != 3:
-    print(
-        'Usage: {0} "input.file" "output.file"'
-        .format(os.path.basename(__file__)))
-    sys.exit()
+class Generator:
+    def __init__(self, file_in, file_out):
+        self._file_in = file_in
+        self._file_out = file_out
+        self._statements = []
+        self._tables = []
 
-in_file = sys.argv[1]
-out_file = sys.argv[2]
-statements = []
-tables = []
+    def create_objects(self):
+        with open(self._file_in, 'r') as f:
+            doc = yaml.load(f)
 
-def create_objects():
-    with open(in_file, 'r') as f:
-        doc = yaml.load(f)
+        for table_name in doc:
+            t = Table(table_name.lower())
+            for fields in doc[table_name]:
+                for field_name in doc[table_name][fields]:
+                    field_value = doc[table_name][fields][field_name].lower()
+                    t._fields[field_name.lower()] = field_value.lower()
+            self._tables.append(t)
 
-    for table_name in doc:
-        t = Table(table_name.lower())
-        for fields in doc[table_name]:
-            for field_name in doc[table_name][fields]:
-                field_value = doc[table_name][fields][field_name].lower()
-                t._fields[field_name.lower()] = field_value.lower()
-        tables.append(t)
+    def create_sql_tables(self):
+        sql_fields = []
 
-def create_sql_tables():
-    sql_fields = []
+        for table in self._tables:
+            for field_name, field_value in table._fields.items():
+                sql_fields.append('\t\"{0}_{1}\" {2}'.format(
+                    table._name, field_name, field_value, ))
+            self._statements.append(CREATE_TABLE.format(table._name, ',\n'.join(sql_fields) + ','))
 
-    for table in tables:
-        for field_name, field_value in table._fields.items():
-            sql_fields.append('\t\"{0}_{1}\" {2}'.format(
-                table._name, field_name, field_value, ))
-        statements.append(CREATE_TABLE.format(table._name, ',\n'.join(sql_fields) + ','))
+    def create_sql_trigers(self):
+        for table in self._tables:
+            self._statements.append(CREATE_TRIGGER.format(table._name))
 
-def create_sql_trigers():
-    for table in tables:
-        statements.append(CREATE_TRIGGER.format(table._name))
+    def write_file(self):
+        with open(self._file_out, 'w') as f:
+            for stmt in self._statements:
+                f.write(stmt)
+        print('SQL query saved to {0}'.format(os.path.abspath(self._file_out)))
 
-def save_statements():
-    with open(out_file, 'w') as f:
-        for stmt in statements:
-            f.write(stmt)
-    print('SQL query saved to {0}'.format(os.path.abspath(out_file)))
-
-create_objects()
-create_sql_tables()
-create_sql_trigers()
-save_statements()
+    def save_statements(self):
+        self.create_objects()
+        self.create_sql_tables()
+        self.create_sql_trigers()
+        self.write_file()
--- output.sql
+++ output.sql
@@ -1,59 +1,58 @@
 CREATE TABLE "author" (
-    "author_id" SERIAL PRIMARY KEY,
+	"author_id" SERIAL PRIMARY KEY,
 	"author_lastname" varchar(64),
 	"author_firstname" varchar(64),
 	"author_age" int,
-    "author_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())        AS INTEGER),
-    "author_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())        AS INTEGER)
-);
-
-CREATE TABLE "article" (
-    "article_id" SERIAL PRIMARY KEY,
-	"author_lastname" varchar(64),
-	"author_firstname" varchar(64),
-	"author_age" int,
-	"article_text" text,
-	"article_title" varchar(50),
-    "article_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())        AS INTEGER),
-    "article_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())        AS INTEGER)
+	"author_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) AS INTEGER),
+	"author_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) AS INTEGER)
 );
 
 CREATE TABLE "category" (
-    "category_id" SERIAL PRIMARY KEY,
+	"category_id" SERIAL PRIMARY KEY,
 	"author_lastname" varchar(64),
 	"author_firstname" varchar(64),
 	"author_age" int,
+	"category_title" varchar(50),
+	"category_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) AS INTEGER),
+	"category_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) AS INTEGER)
+);
+
+CREATE TABLE "article" (
+	"article_id" SERIAL PRIMARY KEY,
+	"author_lastname" varchar(64),
+	"author_firstname" varchar(64),
+	"author_age" int,
+	"category_title" varchar(50),
 	"article_text" text,
 	"article_title" varchar(50),
-	"category_title" varchar(50),
-    "category_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())        AS INTEGER),
-    "category_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now())        AS INTEGER)
+	"article_created" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) AS INTEGER),
+	"article_updated" INTEGER NOT NULL DEFAULT cast(extract(epoch from now()) AS INTEGER)
 );
 
 CREATE OR REPLACE FUNCTION update_author_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-   NEW.author_updated = cast(extract(epoch from now()) as integer);
-   RETURN NEW;
+	NEW.author_updated = cast(extract(epoch from now()) as integer);
+	RETURN NEW;
 END;
 $$ language 'plpgsql';
-CREATE TRIGGER "tr_author_updated" BEFORE UPDATE ON "author" FOR EACH ROW EXECUTE    PROCEDURE update_author_timestamp();
-
-CREATE OR REPLACE FUNCTION update_article_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-   NEW.article_updated = cast(extract(epoch from now()) as integer);
-   RETURN NEW;
-END;
-$$ language 'plpgsql';
-CREATE TRIGGER "tr_article_updated" BEFORE UPDATE ON "article" FOR EACH ROW EXECUTE    PROCEDURE update_article_timestamp();
+CREATE TRIGGER "tr_author_updated" BEFORE UPDATE ON "author" FOR EACH ROW EXECUTE PROCEDURE update_author_timestamp();
 
 CREATE OR REPLACE FUNCTION update_category_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-   NEW.category_updated = cast(extract(epoch from now()) as integer);
-   RETURN NEW;
+	NEW.category_updated = cast(extract(epoch from now()) as integer);
+	RETURN NEW;
 END;
 $$ language 'plpgsql';
-CREATE TRIGGER "tr_category_updated" BEFORE UPDATE ON "category" FOR EACH ROW EXECUTE    PROCEDURE update_category_timestamp();
+CREATE TRIGGER "tr_category_updated" BEFORE UPDATE ON "category" FOR EACH ROW EXECUTE PROCEDURE update_category_timestamp();
+
+CREATE OR REPLACE FUNCTION update_article_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+	NEW.article_updated = cast(extract(epoch from now()) as integer);
+	RETURN NEW;
+END;
+$$ language 'plpgsql';
+CREATE TRIGGER "tr_article_updated" BEFORE UPDATE ON "article" FOR EACH ROW EXECUTE PROCEDURE update_article_timestamp();
 
